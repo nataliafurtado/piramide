6620bbd3c89f808c9eee3df7b49a94cc16ef3dd4
nataliafurtado <jefguns@gmail.com>
2019-11-13 12:02:14 -0300

    kk

--- lib/main.dart
+++ lib/main.dart
@@ -37,7 +37,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+     // title: 'Flutter Demo',
       theme: ThemeData(
           fontFamily: 'OpenSans',
           primarySwatch: Colors.blue,
